a433a3256c53a6a8804bf0958c6330fc11dfa673
Karthik M S <ms18karthik@gmail.com>
2019-10-01 22:28:51 +0530

    passing the first test

---
+++ lib/bottles.rb
@@ -0,0 +1,5 @@
+class Bottles
+    def verse(number)
+       return "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n" 
+    end
+end
